85063f71c85a2a3f8bb11d34b4d45413374625b3
kogliGer <kogli2000@gmail.com>
2016-11-30 19:05:50 +0100

    printing num

--- java/src/javatestproject/JavaTestProject.java
+++ java/src/javatestproject/JavaTestProject.java
@@ -3,8 +3,8 @@ package javatestproject;
 public class JavaTestProject {
 	public static void main(String args[]){
 		System.out.println("hello world");
-
+ 
 		int num = 3;
-
+		System.out.println(num);
 	}
 }
